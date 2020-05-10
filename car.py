a72fea5d5c0b59ae65cfd88c689967ca116ad37c
villysun <53158771+villysun@users.noreply.github.com>
2020-05-10 09:55:09 +0800

    上传文件
    测试上传文件功能

---
+++ car.py
@@ -0,0 +1,12 @@
+cars = ["Buick","Ford","Bmw","Honda","Audi","Toyota"]
+print(cars)
+print(sorted(cars))
+print(cars)
+print(sorted(cars,reverse = True))
+print(cars)
+
+
+cars.append("Nissan")
+print(cars)
+cars.insert(1,"Lincoln")
+print(cars)
